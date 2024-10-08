a468df2f0efb3cec198912e86a27fdb7c70fc856
mashiroissocute <1319494460@qq.com>
2024-10-08 19:55:54 +0800

    addnote 20241008

--- interview-note/python/learn.py
+++ interview-note/python/learn.py
@@ -1,8 +1,20 @@
-from queue import Queue
+import functools
 
-q = Queue()
-q.put(1)  # 向队列中添加元素
-q.put(2)  # 向队列中添加元素
 
-item = q.get()  # 从队列中获取元素
-print(item)
+def repeat(num):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args,**kwargs):
+            for i in range(0,num):
+                func(*args,**kwargs)
+        return wrapper
+    return decorator
+
+
+a =10
+@repeat(a)
+def myprint(msg):
+    print(msg)
+    
+
+myprint("sss")
---
+++ quant_note/docs/prejob/skills.md
@@ -0,0 +1,17 @@
+
+## 内容
+
+- 日终清算
+
+
+## skills
+
+- python
+- clickhouse
+- redis
+- mysql
+- pandas
+- numpy
+- flink
+- k8s
+- 基金托管外包业务，具有基金运营业务相关的TA、估值、资金等1-2个系统经验；
